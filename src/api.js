f87caf9d7dac0d20e763002fd17320fa0d579325
Jeff Appareti <jappareti@gmail.com>
2018-10-26 13:26:17 -0700

    Updated API proxy to be more generic

--- src/api.js
+++ src/api.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 
 const DEFAULT_PARAMS = { "number-adult-passengers": 1 };
+const BASE_URL =
+  "https://mobile.southwest.com/api/mobile-air-booking/v1/mobile-air-booking/page/flights/products";
 
 // Get Flights
 export const getFlights = searchFilters => {
   const params = { ...DEFAULT_PARAMS, ...searchFilters };
+  const queryString = Object.keys(params)
+    .map(key => key + "=" + params[key])
+    .join("&");
+  const url = `${BASE_URL}?${queryString}`;
   return axios.get("/.netlify/functions/getflights", {
-    params,
-    headers: { "content-type": "application/json" }
+    params: {
+      url
+    }
   });
 };
--- src/lambda/getflights.js
+++ src/lambda/getflights.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const X_API_KEY = "l7xx12ebcbc825eb480faa276e7f192d98d1";
-const BASE_URL =
-  "https://mobile.southwest.com/api/extensions/v1/mobile/flights/products";
+const X_API_KEY = "l7xx0a43088fe6254712b10787646d1b298e";
 const USER_AGENT = "Southwest/3.6.16 (iPhone; iOS 9.3.2; Scale/2.00)";
 
 export const handler = (event, context, callback) =>
   axios
-    .get(BASE_URL, {
+    .get(event.queryStringParameters.url, {
       headers: {
         "X-API-Key": X_API_KEY,
         "User-Agent": USER_AGENT,
         Accept: "*/*"
-      },
-      params: event.queryStringParameters
+      }
     })
     .then(results => {
       console.log("========================================================");
       console.log(results);
       if (!results.status) {
-        callback(null, { statusCode: 500, body: "Error fetching results" });
+        callback(null, {
+          statusCode: 500,
+          body: "Error fetching results"
+        });
       } else if (results.status !== 200) {
         callback(null, {
           statusCode: results.status,
@@ -29,7 +29,10 @@ export const handler = (event, context, callback) =>
       } else {
         callback(null, {
           statusCode: results.status,
-          body: JSON.stringify(results.data)
+          body: JSON.stringify(results.data),
+          headers: {
+            "content-type": "application/json"
+          }
         });
       }
     })
